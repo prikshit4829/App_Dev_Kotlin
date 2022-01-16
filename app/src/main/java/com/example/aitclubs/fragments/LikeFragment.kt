3f93d1a0c3b67eac75579f076b8b767c091a841f
prikshit4829 <prikshit4829@gmail.com>
2022-01-17 02:14:40 +0530

    ait club event app

--- app/src/main/java/com/example/aitclubs/fragments/LikeFragment.kt
+++ app/src/main/java/com/example/aitclubs/fragments/LikeFragment.kt
@@ -1,21 +1,38 @@
 package com.example.aitclubs.fragments
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.aitclubs.R
+import com.example.aitclubs.databinding.FragmentLikeBinding
+import java.io.File
 
 class LikeFragment : Fragment() {
 
+  lateinit var binding:FragmentLikeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_like, container, false)
+        binding = FragmentLikeBinding.inflate(layoutInflater,container,false)
+
+//        val allFiles:Array<File>
+//        val imageList= arrayListOf<String>()
+//
+//        val targetPath=Environment.getExternalStorageDirectory().absolutePath+"/Images/Amoled Wallpaper"
+//
+//        val targetFile=File(targetPath)
+//        allFiles= targetFile.listFiles()!!
+//
+//        for (data in allFiles){
+//            imageList.add(data.absolutePath)
+//        }
+
+        return binding.root
     }
 
 
